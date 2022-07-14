988fe08b6e32ca677b4c66ea09bf2b243da559fa
LukitaChan <kamery_94@hotmail.com>
2022-07-14 10:31:18 -0500

    04-fireb-08

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,7 +7,7 @@ const Button = ({ text, type, color = "purple", loading, onClick }) => {
     <button
       onClick={onClick}
       type={type}
-      className={`text-white bg-blue-700 hover:bg-${color}-800 focus:outline-none focus:ring-4 focus:ring-${color}-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-${color}-600 dark:hover:bg-${color}-700 dark:focus:ring-${color}-900`}
+      className={`text-white bg-red-700 hover:bg-${color}-800 focus:outline-none focus:ring-4 focus:ring-${color}-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-${color}-600 dark:hover:bg-${color}-700 dark:focus:ring-${color}-900`}
     >
       {text}
     </button>
