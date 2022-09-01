bbbd7c41b5f68b1bf6a179b4002ae5505561e721
LukitaChan <kamery_94@hotmail.com>
2022-09-01 11:12:32 -0500

    gh pages añadido

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,17 @@ npm run build
 # navigate into the build output directory
 cd dist
 
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
 git init
+git checkout -b main
 git add -A
 git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f https://github.com/LukitaChan/react-firebase9-router6-vite.git main:gh-pages
