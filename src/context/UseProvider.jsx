39524776881e60c5bed20571766c771c6f402872
LukitaChan <kamery_94@hotmail.com>
2022-09-01 09:23:02 -0500

    01092022

--- src/context/UseProvider.jsx
+++ src/context/UseProvider.jsx
@@ -1,13 +1,53 @@
-import { createContext, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { auth } from "../firebase";
 
 export const UserContext = createContext();
 
 const UserProvider = (props) => {
   const [user, setUser] = useState(false);
+  //Para el metodo de deslogueo Web/comenzar/configura el observador
+  useEffect(() => {
+    //ell metodo onAuthStateChanged verificara si el usuario esta autenticado o no.
+    const unsuscribe = onAuthStateChanged(auth, (user) => {
+      console.log(user);
+      if (user) {
+        //destructuramos de user
+        const { email, photoURL, displayName, uid } = user;
+        setUser({ email, photoURL, displayName, uid });
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => {
+      unsuscribe;
+    };
+  }, []);
+
+  //metodo para acceder a los componentes: createUserWithEmailAndPassword.
+  const registerUser = (email, password) =>
+    //web/comenzar/Regitra usuarios nuevos
+    createUserWithEmailAndPassword(auth, email, password);
+
+  const loginUser = (email, password) =>
+    //web/comenzar/Acceso de usuarios existentes
+    signInWithEmailAndPassword(auth, email, password);
+
+  const signOutUser = () => {
+    signOut(auth);
+  };
 
   return (
     //Los Children son componentes que estaran en el interior del Provider.
-    <UserContext.Provider value={{ user, setUser }}>
+    <UserContext.Provider
+      value={{ user, setUser, registerUser, loginUser, signOutUser }}
+    >
       {props.children}
     </UserContext.Provider>
   );
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVPntGNPAzFCQcMhXj4QLBzRC3LcyK-fU",
+  authDomain: "react-2022-1d598.firebaseapp.com",
+  projectId: "react-2022-1d598",
+  storageBucket: "react-2022-1d598.appspot.com",
+  messagingSenderId: "447048588744",
+  appId: "1:447048588744:web:539c5b726f66cce997f4fd",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// auth trae la configuracion del proyecto
+const auth = getAuth(app);
+
+export { auth };
